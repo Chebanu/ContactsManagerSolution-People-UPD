e0d1926076cf7369f579c3494539862a5585592a
Alexei Chebanu <0alexchebanu@gmail.com>
2023-01-11 00:07:15 +0200

    Remote Validation (Email)

--- ContactsManager.Core/DTO/RegisterDTO.cs
+++ ContactsManager.Core/DTO/RegisterDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -13,6 +14,7 @@ namespace ContactsManager.Core.DTO
         public string PersonName { get; set; }
         [Required(ErrorMessage = "Email cannot be blank")]
         [EmailAddress(ErrorMessage = "Email should be in a proper email address")]
+        [Remote(action: "IsEmailAlreadyRegistered", controller: "Account", ErrorMessage = "Email is already in use")]
         public string Email { get; set; }
         [Required(ErrorMessage = "Phone cannot be blank")]
         [RegularExpression("[0-9]*$", ErrorMessage = "Phone number should contain numbers only")]
